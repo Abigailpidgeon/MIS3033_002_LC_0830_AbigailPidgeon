a967bd6f4fbc176f9630de01a1a78a11af6b44d5
Abby Pidgeon <Abigail.L.Pidgeon-1@ou.edu>
2023-08-29 21:14:36 -0500

    Add project files.

---
+++ MIS3033_002_LC_0830_AbigailPidgeon/Program.cs
@@ -0,0 +1,15 @@
+﻿// MIS 3033 002
+// AUG 30 2023
+// Abigail Pidgeon 113515288
+
+string str1; // complex
+str1 = new string("MIS3033");
+str1 = @"M\nIS\t30330";
+str1 = "MIS3033AbigailPidgeon";
+
+int 1;
+1 = str1.Length;
+Console.WriteLine(1);
+
+Console.WriteLine(str1.ToLower());
+Console.WriteLine(str1.Contains("MIS"));
